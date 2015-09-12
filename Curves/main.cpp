28cea0e84eb6024740419bd5f496ce11d6d4ba76
Matthew Dillard <7395051@gmail.com>
2015-09-12 11:29:54 -0500

    Initial Commit

---
+++ Curves/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Curves
+//
+//  Created by Matthew Dillard on 9/12/15.
+//  Copyright (c) 2015 Matthew Dillard. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
